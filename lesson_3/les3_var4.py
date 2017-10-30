891b3fd3db7ebbfdb90872022ceda533fe14136f
Packmanlol <packmangreb@gmail.com>
2017-10-30 18:53:55 +1000

    Fixed all

--- lesson_3/les3_var4.py
+++ lesson_3/les3_var4.py
@@ -1,11 +1,4 @@
 
-def search_for_records(lst, data, col_num):
-    search_rezults=[]
-    for row_num, record in enumerate(lst):
-        if record[col_num] == data:
-            search_rezults.append(record)
-    return search_rezults
-
 try:
     with open("books.txt", "r") as my_file:
         list_books = my_file.readlines()
@@ -16,6 +9,8 @@ except FileNotFoundError:
 #Убираем пробелы и разделители в начале и конце элеметов списка
 #Приводим типы данных
 price_pos = 3
+year_pos = 2
+
 try:
     for row_num, row_data in enumerate(list_books):
         list_books[row_num] = row_data.split(",")
@@ -31,16 +26,17 @@ except ValueError:
 
 #Поиск книг по году изданияи и сортировка результата по алфавиту
 year = input("Введите год издания книг: ")
-lst = search_for_records(list_books, year, 2)
-lst.sort(key=lambda num_str: num_str[1])
+lst = list(filter(lambda serch_rez: serch_rez[year_pos] == year, list_books))
+lst.sort(key=lambda num_str: num_str[year_pos])
 
 if not lst:
     print("Книг не найдено")
 else:
-    print("Найдено ", len(lst), "\n", lst)
+    print("Найдено ", len(lst), '\n',
+          '\n'.join(map(str, lst)))
 
 #сортировка по возрастанию цены
-list_books.sort(key = lambda num_: num_[3])
+list_books.sort(key = lambda num_: num_[price_pos])
 
 #Запись в файл с сортировкой по возрасатнию цены
 with open("books_out.txt", "w") as my_file:
